71cc0cb44a74d5197715f4cf707a03edb079af88
Jose Martins <josemartins88@gmail.com>
2022-10-28 16:25:29 +0200

    make test fail on purpose

--- test/smoke.test.js
+++ test/smoke.test.js
@@ -5,6 +5,6 @@ const app = require('../app')
 describe('get endpoint', () => {
   it('should return 200 on initial endpoint', async () => {
     const res = await request(app).get('/')
-    expect(res.statusCode).toEqual(200)
+    expect(res.statusCode).toEqual(201)
   })
 })
